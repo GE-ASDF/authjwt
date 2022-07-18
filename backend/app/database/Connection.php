4a609f3b49dc89094735fd0d98291f530e5a6ca7
Anderson Souza <andersonsouza007@live.com>
2022-07-18 19:24:53 -0300

    Autenticação JWT

--- backend/app/database/Connection.php
+++ backend/app/database/Connection.php
@@ -8,8 +8,7 @@ class Connection
 {
     public static function connect(){
         try{
-            $db = new PDO("mysql:dbname=cronograma_estudos;host=localhost", "root", "");
-            return $db;
+            return new PDO("mysql:dbname=cronograma_estudos;host=localhost", "root", "");
         }catch(PDOException $e){
             var_dump($e->getMessage());
         }
