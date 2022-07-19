ebd05eeb10450d8fae8b793e976c68e8b093c6b7
Anderson Souza <andersonsouza007@live.com>
2022-07-19 14:29:19 -0300

    Autenticação JWT

--- backend/public/auth.php
+++ backend/public/auth.php
@@ -2,17 +2,16 @@
 
 require '../vendor/autoload.php';
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 2));
-$dotenv->load();
-
 $authorization = $_SERVER["HTTP_AUTHORIZATION"];
 $token = str_replace('Bearer ', '', $authorization);
+
 try{
-    $decoded = JWT::decode($token, $_SERVER["KEY"], ['HS256']);
+    $decoded = JWT::decode($encode, new Key($token, 'HS256'));
     echo json_encode($decoded);
 }catch(Throwable $e){
     if($e->getMessage() == "Expired token"){
--- backend/public/login.php
+++ backend/public/login.php
@@ -3,14 +3,14 @@
 require '../vendor/autoload.php';
 use app\database\Connection;
 use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
 header("Access-Control-Allow-Origin: *");
-
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__, 2));
-$dotenv->load();
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
 
 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-$senha = strip_tags($_POST["senha"]);
+$senha = filter_input(INPUT_POST, "senha");
+
 
 $pdo = Connection::connect();
 $prepare = $pdo->prepare("SELECT * FROM usuarios WHERE email= :email");
@@ -26,9 +26,30 @@ if(!password_verify($senha, $userFound->senha)){
     http_response_code(401);
 }
 
+$key = "adafjlçkj3423";
 $payload = [
     "exp" => time() + 10,
     "iat" => time(),
-    "email"=> $email
+    "email"=> $email,
 ];
-$encode = JWT::encode($payload, $_ENV['KEY'], 'HS256');
+
+$encode = JWT::encode($payload, $key, 'HS256');
+echo json_encode($encode);
+
+if(isset($_SERVER["HTTP_AUTHORIZATION"])){
+
+$authorization = $_SERVER["HTTP_AUTHORIZATION"];
+$token = str_replace('Bearer ', '', $authorization);
+die($authorization);
+try{
+    $decoded = JWT::decode($encode, new Key($token, 'HS256'));
+    echo json_encode($decoded);
+}catch(Throwable $e){
+    if($e->getMessage() == "Expired token"){
+        http_response_code(401);
+        die($e->getMessage());
+    }
+    echo json_encode($e->getMessage());
+}
+
+}
